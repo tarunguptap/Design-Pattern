52c5f8657509740f60fb07e986848d528c6f20cc
tarunguptap <taunguptap@yahoo.com>
2016-09-29 10:03:36 +0530

    Prototype pattern

---
+++ src/main/java/com/creational/prototype/PrototypePatternDemo.java
@@ -0,0 +1,14 @@
+package com.creational.prototype;
+
+public class PrototypePatternDemo {
+	   public static void main(String[] args) {
+	      ShapeCache.loadCache();
+
+	      Shape clonedShape = (Shape) ShapeCache.getShape("1");
+	      System.out.println("Shape : " + clonedShape.getType());		
+
+	      Shape clonedShape2 = (Shape) ShapeCache.getShape("2");
+	      System.out.println("Shape : " + clonedShape2.getType());		
+
+	   }
+	}
